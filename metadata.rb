e51e9ea785137f2a31316109d06710303521c0c5
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-04-22 17:34:28 +0400

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name 'themis-finals-checker-app-py-lib'
 description 'Installs and configures themis.finals.checker.app package'
-version '1.1.1'
+version '1.1.2'
 
 recipe 'themis-finals-checker-app-py-lib',
        'Installs and configures themis.finals.checker.app package'
-depends 'latest-git', '~> 1.1.11'
+depends 'git', '~> 6.0.0'
 depends 'git2', '~> 1.0.0'
-depends 'ssh-private-keys', '~> 1.0.0'
+depends 'ssh-private-keys', '~> 2.0.0'
+depends 'ssh_known_hosts', '~> 5.1.0'
